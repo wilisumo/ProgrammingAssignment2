dfab0df7da9fd7431df5bd26a2679a8ed08fbfb2
wilisumo <wilisumo@gmail.com>
2015-01-25 17:35:07 -0500

    ##first function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+##Function that create a matrix object than cache it's inverse
 makeCacheMatrix <- function(x = matrix()) {
 
+        i<- Null
+        
+        set<- function(y){
+                x<<-y
+                i<<-Null
+                
+        }
+        get<- function() x
+        setinverse<-function(solve) i<<-solve
+        getinverse <-function() i
+        list(set=set,get=get, setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## function that computes invers of a given matrix retuned by makeCacheMatrix function
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
